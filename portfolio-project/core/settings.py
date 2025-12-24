8b7e6919bf48a1fcf71c3a0c009ca1ebf7428836
sidi <kkssndregjoni@gmail.com>
2025-12-24 16:55:58 +0100

    fixes on settings.py backend

--- portfolio-project/core/settings.py
+++ portfolio-project/core/settings.py
@@ -40,10 +40,10 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'portfolio_api',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,8 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 CORS_ALLOWED_ORIGINS = [
