8bc82e9a289b9f06c1e014ca251a1809d43e451e
sidi <kkssndregjoni@gmail.com>
2025-12-24 19:45:58 +0100

    navbar modifications

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <nav className="navbar">
             <div className="container navbar-content">
                 <Link to="/" className="navbar-logo">
                     QS<span>MUN</span>
                 </Link>
-                <div className="navbar-links">
-                    <Link to="/">Kreu</Link>
-                    <Link to="/about">Rreth Nesh</Link>
-                    <Link to="/game">Simulator</Link> {/* New Link */}
-                    <Link to="/apply">Apliko</Link>
+
+                {/* Hamburger Icon */}
+                <div className={`hamburger ${isOpen ? 'active' : ''}`} onClick={toggleMenu}>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </div>
+
+                {/* Navbar Links */}
+                <div className={`navbar-links ${isOpen ? 'active' : ''}`}>
+                    <Link to="/" onClick={() => setIsOpen(false)}>Kreu</Link>
+                    <Link to="/about" onClick={() => setIsOpen(false)}>Rreth Nesh</Link>
+                    <Link to="/game" onClick={() => setIsOpen(false)}>Simulator</Link>
+                    <Link to="/apply" onClick={() => setIsOpen(false)}>Apliko</Link>
                 </div>
             </div>
         </nav>
