b35440f5d55dda452defb49bd48fd7a78bfda789
sidi <kkssndregjoni@gmail.com>
2025-12-24 17:54:12 +0100

    backend fix

--- portfolio-project/portfolio_api/models.py
+++ portfolio-project/portfolio_api/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class Event(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    location = models.CharField(max_length=200, default="Ismail Qemali Hall")
+    # Updated default location below
+    location = models.CharField(max_length=200, default="Qemal Stafa")
     date = models.DateTimeField()
     image = models.ImageField(upload_to='events/', blank=True, null=True)
     registration_link = models.URLField(blank=True, null=True)
