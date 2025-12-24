8613bf771757de4581a18b1ec34d27f6edeb348b
sidi <kkssndregjoni@gmail.com>
2025-12-24 18:29:39 +0100

    fixes again

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <nav className="navbar">
             <div className="container navbar-content">
                 <Link to="/" className="navbar-logo">
                     QS<span>MUN</span>
                 </Link>
-                <div className="navbar-links">
-                    <Link to="/">Kreu</Link>
-                    <Link to="/about">Rreth Nesh</Link>
-                    <Link to="/game">Simulator</Link> {/* New Link */}
-                    <Link to="/apply">Apliko</Link>
+                
+                {/* Mobile Toggle */}
+                <div className={`menu-toggle ${isOpen ? 'active' : ''}`} onClick={() => setIsOpen(!isOpen)}>
+                    <span></span><span></span><span></span>
+                </div>
+
+                <div className={`navbar-links ${isOpen ? 'show' : ''}`}>
+                    <Link to="/" onClick={() => setIsOpen(false)}>Kreu</Link>
+                    <Link to="/about" onClick={() => setIsOpen(false)}>Rreth Nesh</Link>
+                    <Link to="/game" onClick={() => setIsOpen(false)}>Simulator</Link>
+                    <Link to="/apply" className="nav-apply-btn" onClick={() => setIsOpen(false)}>Apliko</Link>
                 </div>
             </div>
         </nav>
