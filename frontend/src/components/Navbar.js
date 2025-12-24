b3f9027a2f2bea3a83ae500c5b457cfc920e99a7
sidi <kkssndregjoni@gmail.com>
2025-12-24 19:43:09 +0100

    fixes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,27 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
-
     return (
         <nav className="navbar">
             <div className="container navbar-content">
                 <Link to="/" className="navbar-logo">
                     QS<span>MUN</span>
                 </Link>
-                
-                {/* Mobile Toggle */}
-                <div className={`menu-toggle ${isOpen ? 'active' : ''}`} onClick={() => setIsOpen(!isOpen)}>
-                    <span></span><span></span><span></span>
-                </div>
-
-                <div className={`navbar-links ${isOpen ? 'show' : ''}`}>
-                    <Link to="/" onClick={() => setIsOpen(false)}>Kreu</Link>
-                    <Link to="/about" onClick={() => setIsOpen(false)}>Rreth Nesh</Link>
-                    <Link to="/game" onClick={() => setIsOpen(false)}>Simulator</Link>
-                    <Link to="/apply" className="nav-apply-btn" onClick={() => setIsOpen(false)}>Apliko</Link>
+                <div className="navbar-links">
+                    <Link to="/">Kreu</Link>
+                    <Link to="/about">Rreth Nesh</Link>
+                    <Link to="/game">Simulator</Link> {/* New Link */}
+                    <Link to="/apply">Apliko</Link>
                 </div>
             </div>
         </nav>
